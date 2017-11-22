166691adf8f6fef16a3f6e7f143d3439b7394c81
studentA <studentA@example.com>
2017-11-23 02:13:17 +0300

    skeleton

---
+++ program/program/program.cpp
@@ -0,0 +1,15 @@
+// program.cpp : Defines the entry point for the console application.
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//cout << MySum(10, 3) << endl;
+	//cout << MySub(10, 3) << endl;
+	//cout << MyMul(10, 3) << endl;
+	//cout << MyDiv(10, 3) << endl;
+	//cout << MyPow(10.5, 3.7) << endl;
+	system("pause");
+	return 0;
+}
